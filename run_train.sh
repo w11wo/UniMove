9029fd3b680317b5a7b69c46e63b3bbb4191b638
w11wo <wilsonwong961@gmail.com>
2025-09-02 16:26:42 +1000

    Train on 7 cities

--- run_train.sh
+++ run_train.sh
@@ -1,8 +1,24 @@
-python main.py \
-    --device cuda:0 \
-    --city melbourne \
-    --target_city melbourne \
-    --train_root traj_dataset/massive_steps/train \
-    --val_root traj_dataset/massive_steps/val \
-    --test_root traj_dataset/massive_steps/test \
-    --B 8
+for city in beijing melbourne shanghai sydney tokyo; do
+    python main.py \
+        --device cuda:0 \
+        --city $city \
+        --target_city $city \
+        --train_root traj_dataset/massive_steps/train \
+        --val_root traj_dataset/massive_steps/val \
+        --test_root traj_dataset/massive_steps/test \
+        --B 8
+done
+
+for city in kuwait_city moscow; do
+    python main.py \
+        --device cuda:0 \
+        --city $city \
+        --target_city $city \
+        --train_root traj_dataset/massive_steps/train \
+        --val_root traj_dataset/massive_steps/val \
+        --test_root traj_dataset/massive_steps/test \
+        --B 4
+done
+
+# for city in istanbul jakarta new_york petaling_jaya sao_paulo; do
+# done
