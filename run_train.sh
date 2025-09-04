de9d9048dca129e2a793eb30f313af132098caf1
w11wo <wilsonwong961@gmail.com>
2025-09-04 03:28:22 +0000

    Unified hyperparams

--- run_train.sh
+++ run_train.sh
@@ -1,15 +1,4 @@
-for city in beijing melbourne shanghai sydney tokyo; do
-    python main.py \
-        --device cuda:0 \
-        --city $city \
-        --target_city $city \
-        --train_root traj_dataset/massive_steps/train \
-        --val_root traj_dataset/massive_steps/val \
-        --test_root traj_dataset/massive_steps/test \
-        --B 8
-done
-
-for city in istanbul kuwait_city moscow new_york petaling_jaya sao_paulo; do
+for city in beijing istanbul jakarta kuwait_city melbourne moscow new_york petaling_jaya sao_paulo shanghai sydney tokyo; do
     python main.py \
         --device cuda:0 \
         --city $city \
@@ -19,6 +8,3 @@ for city in istanbul kuwait_city moscow new_york petaling_jaya sao_paulo; do
         --test_root traj_dataset/massive_steps/test \
         --B 4
 done
-
-# for city in  jakarta; do
-# done
